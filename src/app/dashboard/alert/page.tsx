c55c8dc52a681aab53382e80d85e34491b3c9f70
Humbert Dominguez <humbertodelac13@gmail.com>
2025-04-22 11:08:29 -0600

    alert con variants

--- src/app/dashboard/alert/page.tsx
+++ src/app/dashboard/alert/page.tsx
@@ -1,3 +1,38 @@
+import { Terminal, Check, X } from "lucide-react";
+import { Alert, AlertDescription, AlertTitle } from "@/components/ui/alert";
+
 export default function Page() {
-  return <div>Hola mundo desde alert</div>;
+  return (
+    <div className="grid gap-3">
+      <h1>Alert page</h1>
+
+      <hr />
+      <Alert>
+        <Terminal className="h-4 w-4" />
+        <AlertTitle>Heads up!</AlertTitle>
+        <AlertDescription>
+          You can add components to your app using the cli.
+        </AlertDescription>
+      </Alert>
+
+      <hr />
+      <Alert variant="destructive">
+        <X className="h-4 w-4" />
+        <AlertTitle>Heads up [destructive variant]!</AlertTitle>
+        <AlertDescription>
+          You can add components to your app using the cli.
+        </AlertDescription>
+      </Alert>
+
+      <hr />
+      <Alert variant="success">
+        <Check className="h-4 w-4" />
+        <AlertTitle>Heads up [success | custom variant]!</AlertTitle>
+        <AlertDescription>
+          You can add components to your app using the cli.
+        </AlertDescription>
+      </Alert>
+
+    </div>
+  );
 }
