cf6aaa820ec2e098fb9cbe320defc71e38f7f303
Humbert Dominguez <humbertodelac13@gmail.com>
2025-04-27 18:20:15 -0600

    data table: acciones de la fila

--- src/app/dashboard/data-table/columns.tsx
+++ src/app/dashboard/data-table/columns.tsx
@@ -4,14 +4,29 @@ import { Badge } from "@/components/ui/badge";
 import { Payment } from "@/data/payments.data";
 import { ColumnDef } from "@tanstack/react-table";
 
+import { Button } from "@/components/ui/button";
+import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuLabel, DropdownMenuSeparator, DropdownMenuTrigger } from "@/components/ui/dropdown-menu";
+import { MoreHorizontal } from "lucide-react";
+import { toast } from "sonner";
+
 export const columns: ColumnDef<Payment>[] = [
   {
     accessorKey: "clientName",
-    header: "Nombre Cliente",
+    header: () => <div className="text-center">Nombre Cliente</div>,
+    cell: ({ row }) => {
+      const value: string = row.getValue("clientName") as string;
+
+      return <div className="text-center">{value}</div>;
+    },
   },
   {
     accessorKey: "email",
-    header: "Correo",
+    header: () => <div className="text-center">Nombre Cliente</div>,
+    cell: ({ row }) => {
+      const value: string = row.getValue("email") as string;
+
+      return <div className="text-center">{value}</div>;
+    },
   },
   {
     accessorKey: "status",
@@ -31,7 +46,9 @@ export const columns: ColumnDef<Payment>[] = [
 
       return (
         <div className="flex justify-center">
-          <Badge variant={variant} capitalize >{status}</Badge>
+          <Badge variant={variant} capitalize>
+            {status}
+          </Badge>
         </div>
       );
     },
@@ -52,6 +69,45 @@ export const columns: ColumnDef<Payment>[] = [
 
       // retornamos en la celda, el valor formateado
       return <div className="text-center font-medium">{formatted}</div>;
+    },
+  },
+  // para las acciones no vamos a poner un header
+  {
+    id: "actions",
+    cell: ({ row }) => {
+      // obtenemos la información de la fila en 'original', es decir sin cambios que hayan ocurrido en cada celda
+      const payment = row.original;
+
+      return (
+        <DropdownMenu>
+          <DropdownMenuTrigger asChild>
+            <Button variant="ghost" className="h-8 w-8 p-0">
+              <span className="sr-only">Open menu</span>
+              <MoreHorizontal className="h-4 w-4" />
+            </Button>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent align="end">
+            <DropdownMenuLabel>Actions</DropdownMenuLabel>
+            <DropdownMenuItem className="cursor-pointer"
+              onClick={() => {
+                const promise = navigator.clipboard.writeText(payment.id);
+
+                toast.promise(promise, {
+                  loading: "copiando...",
+                  success: `Se ha copiado el id al portapapeles!`,
+                  error: "error...",
+                  position: "top-right"
+                });
+              }}
+            >
+              Copy payment ID
+            </DropdownMenuItem>
+            <DropdownMenuSeparator />
+            <DropdownMenuItem>View customer</DropdownMenuItem>
+            <DropdownMenuItem>View payment details</DropdownMenuItem>
+          </DropdownMenuContent>
+        </DropdownMenu>
+      );
     },
   },
 ];
