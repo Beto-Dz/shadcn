c174078120239a4771847a17999479a853caf436
Humbert Dominguez <humbertodelac13@gmail.com>
2025-04-27 19:56:23 -0600

    data table: filtrar registros

--- src/app/dashboard/data-table/data-table.tsx
+++ src/app/dashboard/data-table/data-table.tsx
@@ -1,9 +1,23 @@
 "use client";
 
-import { ColumnDef, flexRender, getCoreRowModel, useReactTable, getPaginationRowModel, SortingState, getSortedRowModel } from "@tanstack/react-table";
+import {
+  ColumnDef,
+  flexRender,
+  getCoreRowModel,
+  useReactTable,
+  getPaginationRowModel,
+  SortingState,
+  getSortedRowModel,
+  ColumnFiltersState,
+  getFilteredRowModel,
+  Table as dTable,
+} from "@tanstack/react-table";
 
 import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from "@/components/ui/table";
+import { Select, SelectContent, SelectGroup, SelectItem, SelectLabel, SelectTrigger, SelectValue,} from "@/components/ui/select"
+
 import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
 import { useState } from "react";
 
 interface DataTableProps<TData, TValue> {
@@ -11,26 +25,91 @@ interface DataTableProps<TData, TValue> {
   data: TData[];
 }
 
-export function DataTable<TData, TValue>({ columns, data }: DataTableProps<TData, TValue>) {
+const InputFilterComponent = ({ table, placeholder, columnName }: { table: dTable<any>; placeholder: string; columnName: string; }) => {
+  return (
+    <Input
+      placeholder={placeholder}
+      value={(table.getColumn(columnName)?.getFilterValue() as string) ?? ""}
+      onChange={(event) => table.getColumn(columnName)?.setFilterValue(event.target.value)}
+      className="max-w-sm"
+    />
+  );
+};
 
+const SelectFilterComponent = ( { table, }: { table: dTable<any>;}) => {
+
+  const [value, setValue] = useState<string>("all");
+
+  return (
+    <Select value={value} onValueChange={ (newValue) => {
+      setValue(newValue);
+
+      if( newValue === 'all' ){
+        table.getColumn("status")?.setFilterValue(undefined)
+        return;
+      }
+
+      table.getColumn("status")?.setFilterValue(newValue)
+    } } >
+      <SelectTrigger className="w-[180px]">
+        <SelectValue placeholder="Estatus - all" />
+      </SelectTrigger>
+      <SelectContent>
+        <SelectGroup>
+          <SelectLabel>Estatus</SelectLabel>
+          <SelectItem value="all">all</SelectItem>
+          <SelectItem value="pending">pending</SelectItem>
+          <SelectItem value="processing">processing</SelectItem>
+          <SelectItem value="failed">failed</SelectItem>
+          <SelectItem value="success">success</SelectItem>
+        </SelectGroup>
+      </SelectContent>
+    </Select>
+  );
+};
+
+export function DataTable<TData, TValue>({ columns, data, }: DataTableProps<TData, TValue>) {
   // estado para el ordenamiento
-  const [ sorting, setSorting ] = useState<SortingState>([]);
+  const [sorting, setSorting] = useState<SortingState>([]);
+
+  // estado para filtrar dinamicamente
+  const [columnFilters, setColumnFilters] = useState<ColumnFiltersState>([]);
 
   // hook para la tabla
-  const table = useReactTable({ 
+  const table = useReactTable({
     data,
-    columns, 
+    columns,
     getCoreRowModel: getCoreRowModel(),
-    getPaginationRowModel: getPaginationRowModel(), 
+    getPaginationRowModel: getPaginationRowModel(),
     onSortingChange: setSorting,
     getSortedRowModel: getSortedRowModel(),
+    onColumnFiltersChange: setColumnFilters,
+    getFilteredRowModel: getFilteredRowModel(),
     state: {
       sorting,
-    }, 
+      columnFilters,
+    },
   });
 
   return (
-    <>
+    <div className="flex flex-col gap-2">
+      <h2 className="font-semibold text-2xl">Tabla con tanstack table</h2>
+
+      <div className="flex justify-evenly bg-emerald-50">
+        <span>Filtros:</span>
+        <InputFilterComponent
+          table={table}
+          placeholder="Filtrar nombres..."
+          columnName="clientName"
+        />
+        <InputFilterComponent
+          table={table}
+          placeholder="Filtrar correos..."
+          columnName="email"
+        />
+        <SelectFilterComponent table={table} />
+      </div>
+
       <div className="rounded-md border">
         <Table className="text-center">
           <TableHeader>
@@ -78,7 +157,7 @@ export function DataTable<TData, TValue>({ columns, data }: DataTableProps<TData
                   colSpan={columns.length}
                   className="h-24 text-center"
                 >
-                  No results.
+                  No hay resultados...
                 </TableCell>
               </TableRow>
             )}
@@ -105,7 +184,6 @@ export function DataTable<TData, TValue>({ columns, data }: DataTableProps<TData
           Next
         </Button>
       </div>
-
-    </>
+    </div>
   );
 }
