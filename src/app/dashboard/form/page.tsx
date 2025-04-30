c1449d8ce7af2d89542af22d51bb5bfab59381e6
Humbert Dominguez <humbertodelac13@gmail.com>
2025-04-30 09:10:35 -0600

    form: radio group

--- src/app/dashboard/form/page.tsx
+++ src/app/dashboard/form/page.tsx
@@ -5,14 +5,31 @@ import { zodResolver } from "@hookform/resolvers/zod";
 import { useForm } from "react-hook-form";
 
 import { Button } from "@/components/ui/button";
-import { Form, FormControl, FormDescription, FormField, FormItem, FormLabel, FormMessage } from "@/components/ui/form";
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
+import { RadioGroup, RadioGroupItem } from "@/components/ui/radio-group";
 
 const Page = () => {
   // esquema de zod para validar el formulario
   const formSchema = z.object({
-    username: z.string().min(2, 'Mínimo 2 caracteres').max(20, 'Máximo 20 caracteres'),
-    email: z.string().email('Email inválido').min(1, 'Campo requerido').max(50, 'Máximo 50 caracteres'),
+    username: z
+      .string()
+      .min(2, "Mínimo 2 caracteres")
+      .max(20, "Máximo 20 caracteres"),
+    email: z
+      .string()
+      .email("Email inválido")
+      .min(1, "Campo requerido")
+      .max(50, "Máximo 50 caracteres"),
+    gender: z.enum(["male", "female"], { required_error: "Campo requerido", message: "Seleccione el genero, hombre o mujer." }),
   });
 
   // 1. Define your form.
@@ -21,6 +38,7 @@ const Page = () => {
     defaultValues: {
       username: "",
       email: "",
+      gender: "male",
     },
   });
 
@@ -32,7 +50,7 @@ const Page = () => {
   }
 
   return (
-    <div >
+    <div>
       <Form {...form}>
         <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
           <FormField
@@ -44,9 +62,7 @@ const Page = () => {
                 <FormControl>
                   <Input type="text" placeholder="shadcn" {...field} />
                 </FormControl>
-                <FormDescription>
-                  Nombre público de tu cuenta.
-                </FormDescription>
+                <FormDescription>Nombre público de tu cuenta.</FormDescription>
                 {/* mensaje de error */}
                 <FormMessage />
               </FormItem>
@@ -59,7 +75,11 @@ const Page = () => {
               <FormItem>
                 <FormLabel>Correo electrónico</FormLabel>
                 <FormControl>
-                  <Input type="email" placeholder="shadcn@gmail.com" {...field} />
+                  <Input
+                    type="email"
+                    placeholder="shadcn@gmail.com"
+                    {...field}
+                  />
                 </FormControl>
                 <FormDescription>
                   Correo electrónico de tu cuenta
@@ -69,6 +89,50 @@ const Page = () => {
               </FormItem>
             )}
           />
+
+          <FormField
+            control={form.control}
+            name="gender"
+            render={({ field }) => (
+              <FormItem className="space-y-3">
+                <FormLabel>Genero</FormLabel>
+                <FormControl>
+                  <RadioGroup
+                    onValueChange={field.onChange}
+                    defaultValue={field.value}
+                    className="flex flex-col space-y-1"
+                  >
+                    <FormItem className="flex items-center space-x-3 space-y-0">
+                      <FormControl>
+                        <RadioGroupItem value="xxx" />
+                      </FormControl>
+                      <FormLabel className="font-normal">
+                        Seleccione
+                      </FormLabel>
+                    </FormItem>
+                    <FormItem className="flex items-center space-x-3 space-y-0">
+                      <FormControl>
+                        <RadioGroupItem value="female" />
+                      </FormControl>
+                      <FormLabel className="font-normal">
+                        Female
+                      </FormLabel>
+                    </FormItem>
+                    <FormItem className="flex items-center space-x-3 space-y-0">
+                      <FormControl>
+                        <RadioGroupItem value="male" />
+                      </FormControl>
+                      <FormLabel className="font-normal">
+                        Male
+                      </FormLabel>
+                    </FormItem>
+                  </RadioGroup>
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+
           <Button type="submit">Submit</Button>
         </form>
       </Form>
