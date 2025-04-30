d0f49d41fa55122c006d5cf128d996026c885fed
Humbert Dominguez <humbertodelac13@gmail.com>
2025-04-30 09:26:17 -0600

    form: date picker

--- src/app/dashboard/form/page.tsx
+++ src/app/dashboard/form/page.tsx
@@ -16,6 +16,16 @@ import {
 } from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
 import { RadioGroup, RadioGroupItem } from "@/components/ui/radio-group";
+import { format } from "date-fns";
+import { cn } from "@/lib/utils"
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from "@/components/ui/popover";
+import { toast } from "sonner";
+import { Calendar } from "@/components/ui/calendar";
+import { CalendarIcon } from "lucide-react";
 
 const Page = () => {
   // esquema de zod para validar el formulario
@@ -29,7 +39,13 @@ const Page = () => {
       .email("Email inválido")
       .min(1, "Campo requerido")
       .max(50, "Máximo 50 caracteres"),
-    gender: z.enum(["male", "female"], { required_error: "Campo requerido", message: "Seleccione el genero, hombre o mujer." }),
+    gender: z.enum(["male", "female"], {
+      required_error: "Campo requerido",
+      message: "Seleccione el genero, hombre o mujer.",
+    }),
+    dateOfBirth: z.date({
+      required_error: "fecha de nacimiento requerida",
+    }),
   });
 
   // 1. Define your form.
@@ -39,6 +55,7 @@ const Page = () => {
       username: "",
       email: "",
       gender: "male",
+      dateOfBirth: new Date(),
     },
   });
 
@@ -47,6 +64,13 @@ const Page = () => {
     // Do something with the form values.
     // ✅ This will be type-safe and validated. ( type-saved quiere decir que si se espera un tipo de dato se mantiene ese tipo de dato )
     console.log({ values });
+    toast("You submitted the following values:", {
+      description: (
+        <pre className="mt-2 w-[340px] rounded-md bg-slate-950 p-4">
+          <code className="text-white">{JSON.stringify(values, null, 2)}</code>
+        </pre>
+      ),
+    });
   }
 
   return (
@@ -106,28 +130,66 @@ const Page = () => {
                       <FormControl>
                         <RadioGroupItem value="xxx" />
                       </FormControl>
-                      <FormLabel className="font-normal">
-                        Seleccione
-                      </FormLabel>
+                      <FormLabel className="font-normal">Seleccione</FormLabel>
                     </FormItem>
                     <FormItem className="flex items-center space-x-3 space-y-0">
                       <FormControl>
                         <RadioGroupItem value="female" />
                       </FormControl>
-                      <FormLabel className="font-normal">
-                        Female
-                      </FormLabel>
+                      <FormLabel className="font-normal">Female</FormLabel>
                     </FormItem>
                     <FormItem className="flex items-center space-x-3 space-y-0">
                       <FormControl>
                         <RadioGroupItem value="male" />
                       </FormControl>
-                      <FormLabel className="font-normal">
-                        Male
-                      </FormLabel>
+                      <FormLabel className="font-normal">Male</FormLabel>
                     </FormItem>
                   </RadioGroup>
                 </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+
+          <FormField
+            control={form.control}
+            name="dateOfBirth"
+            render={({ field }) => (
+              <FormItem className="flex flex-col">
+                <FormLabel>Fecha de nacimiento</FormLabel>
+                <Popover>
+                  <PopoverTrigger asChild>
+                    <FormControl>
+                      <Button
+                        variant={"outline"}
+                        className={cn(
+                          "w-[240px] pl-3 text-left font-normal",
+                          !field.value && "text-muted-foreground"
+                        )}
+                      >
+                        {field.value ? (
+                          format(field.value, "PPP")
+                        ) : (
+                          <span>Pick a date</span>
+                        )}
+                        <CalendarIcon className="ml-auto h-4 w-4 opacity-50" />
+                      </Button>
+                    </FormControl>
+                  </PopoverTrigger>
+                  <PopoverContent className="w-auto p-0" align="start">
+                    <Calendar
+                      mode="single"
+                      selected={field.value}
+                      onSelect={field.onChange}
+                      // se deshabilita si la fecha es menor a 1900-01-01
+                      disabled={(date) => date < new Date("1900-01-01")}
+                      initialFocus
+                    />
+                  </PopoverContent>
+                </Popover>
+                <FormDescription>
+                  Tu fecha de nacimiento es utilizada para verificar tu edad.
+                </FormDescription>
                 <FormMessage />
               </FormItem>
             )}
